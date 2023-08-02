926bc96def976635d2616d4cfc817b0efc6c4c70
6N2P <umnik1985@mail.ru>
2023-08-02 12:43:10 +0500

    first commit

---
+++ WebEnterpriseProjectsKMD/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebEnterpriseProjectsKMD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
