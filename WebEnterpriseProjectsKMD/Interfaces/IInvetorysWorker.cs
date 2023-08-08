52de070fbc3a05a44bc2caffc80d07f52837a515
6N2P <umnik1985@mail.ru>
2023-08-08 17:48:10 +0500

    Начал вррстать окно создание инвентарного

---
+++ WebEnterpriseProjectsKMD/Interfaces/IInvetorysWorker.cs
@@ -0,0 +1,9 @@
+﻿namespace WebEnterpriseProjectsKMD.Interfaces
+{
+    public interface IInvetorysWorker
+    {
+        public void CreateInventory();
+        public void UpdateInventory(int id,string n);
+        public void DeleteInventory(int id);
+    }
+}
